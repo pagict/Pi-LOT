fa5ad89e0b00eabd08a42ad72c7da362b17f997d
pagict <pagict@gmail.com>
2014-04-07 22:32:14 +0800

    add message tab class and in storyBoard

---
+++ Pi-LOT/PiMessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PiMessagesViewController.h
+//  Pi-LOT
+//
+//  Created by Peng Pagict on 4/7/14.
+//  Copyright (c) 2014 pagict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PiMessagesViewController : UITableViewController
+
+@end
